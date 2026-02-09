2c5b1c71a31735866d05f546c06a8bfaf37e7569
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-09 12:46:29 +0000

    done act 3

--- session-1/task3/hello2.c
+++ session-1/task3/hello2.c
@@ -3,8 +3,8 @@
  * Hello world program
  * - compilation errors
  */
-
-
+/*this is a comment, the #include <stdio.h> wasn't there originally*/
+#include <stdio.h>
 
 int main( void ) {
     
--- session-1/task3/hello4.c
+++ session-1/task3/hello4.c
@@ -10,4 +10,3 @@ int main( void )
     { printf("Hello\n");
 return 0; }	
     
- 
