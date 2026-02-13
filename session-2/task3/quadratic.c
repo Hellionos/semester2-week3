ebfd81f16a35a77d8ba399175fdafbb88d3bc2c1
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-13 10:48:23 +0000

    done quadractic kinda

--- session-2/task3/quadratic.c
+++ session-2/task3/quadratic.c
@@ -15,14 +15,30 @@ int main( void ) {
     float b = -5.0;
     float c = 6.0;
     float root1, root2;
+    float term1;
+    float term2;
+    float term3;
+    float term2_3;
+    float term4;
+    float term5;
+
 
     /*
      * Implement the formula for the 2 roots of a quadratic.
      * You can define additional variable for intermediate results to make the code simpler.
      * Print out the final results for the 2 roots as float values.
      */
-
+    term1=-b;
+    term2=b*b;
+    term3=-4*a*c;
+    term2_3=term2+term3;
+    term4=sqrt(term2_3);
+    term5=2*a;
+    root1 = (term1+term4)/term5;
+    root2 = (term1-term4)/term5;
     
+    printf("%f\n",root1);
+    printf("%f\n",root2);
 
     return 0;
 }
