4f6d58263faf45fcc7847bbbf041c2b58d6bf369
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-13 09:56:04 +0000

    done sizes task

--- session-2/task1/sizes.c
+++ session-2/task1/sizes.c
@@ -10,11 +10,17 @@ int main( void ) {
 
     char testChar;
     int testInt;
+    short int testshint;
+    long int testloint;
     float testFloat;
+    double testdouble;
 
     printf("Char size is %ld bytes\n",sizeof(testChar));
     printf("Int size is %ld bytes\n",sizeof(testInt));
+    printf("Short Int size is %ld bytes\n",sizeof(testshint));
+    printf("Long Int size is %ld bytes\n",sizeof(testloint));
     printf("Float size is %ld bytes\n",sizeof(testFloat));
+    printf("Double size is %ld bytes\n",sizeof(testdouble));
 
     return 0;
 }
