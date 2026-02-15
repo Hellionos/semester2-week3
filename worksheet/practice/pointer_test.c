9da5e460502016716ed4c8c7beb4f626b08c66b3
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-15 14:32:29 +0000

    done pointer test

--- worksheet/practice/pointer_test.c
+++ worksheet/practice/pointer_test.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /*
- * Exaple using pointers and dereferencing
+ * Example using pointers and dereferencing
  */
 
 int main( void ) {
@@ -11,15 +11,18 @@ int main( void ) {
     int k = -2;
 
     float *pf; // assign the pointer so that it can access the value of f
-    float *pk; // assign the pointer so taht is can access the value of k
+    int *pk; // assign the pointer so taht is can access the value of k
 
-    sum = 0.0; // compute the sum of the primitive values with appropriate casting
+    float sum = f+k; // compute the sum of the primitive values with appropriate casting
 
     printf("%f\n", sum );
 
-    sum = 0.0; // compute the sum via the pointers, using appropriate casting
+    pf = &f;
+    pk = &k;
+
+    float sumptr = (*pf)+(float)(*pk); // compute the sum via the pointers, using appropriate casting
     
-    printf("%f\n", sum );  // implement this using the pointers to print the same value
+    printf("%f\n", sumptr );  // implement this using the pointers to print the same value
 
     return 0;
 }
