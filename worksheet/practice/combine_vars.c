0f995d46a0d7666f0558b1f26ed612c81eacac13
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-15 14:18:57 +0000

    done combine vars

--- worksheet/practice/combine_vars.c
+++ worksheet/practice/combine_vars.c
@@ -14,9 +14,17 @@ int main( void ) {
     // you can verify sums by comparing to a calculator
 
     // compute and print d/k as a double to 16 d.p.
+    double doverk = d/k;
+    printf("D divided K is %.16f\n",(double)doverk);
     // compute and print f-d as a double to 16 d.p.
+    double fminusdd = f-d;
+    printf("F minus D as a double is %.16f\n",fminusdd);
     // compute and print f-d as a float to 16 d.p.
+    float fminusdf = (float)(double)f-d;
+    printf("F minus D as a float is %.16f\n",fminusdf); 
     // compute and print k+d as a double to 16 d.p.
+    double kplusd = (double)k+d;
+    printf("K plus D is %.16f\n",kplusd);
 
     return 0;
 }
